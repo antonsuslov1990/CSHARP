e65d2e5e28d6d203085b30808941c374956d9200
anton <antonsuslov.90@mail.ru>
2022-12-19 22:10:19 +0300

    done task4

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+int i = 2;
+for ( i = m; i <= n; i++)
+ if ( i % 2 == 0) {
+	Console.WriteLine(i);
+ }
+Console.WriteLine($"Это все четные числа из указанного числа: {n}");
