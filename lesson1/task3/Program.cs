ecd43b4498582646927da1528452d15b6a1aec52
anton <antonsuslov.90@mail.ru>
2022-12-19 22:02:10 +0300

    done task3

---
+++ lesson1/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+	Console.WriteLine($"Это число является четным: {n}");
+} 
+else
+{
+	Console.WriteLine($"Число {n} является нечетным!/ Загадайте другое.");
+}
+
+
