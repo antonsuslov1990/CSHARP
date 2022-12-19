4616a13e2203cd4220780d17d1e90fab4b868005
anton <antonsuslov.90@mail.ru>
2022-12-19 21:50:59 +0300

    done task2

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new Random().Next (1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next (1, 100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next (1, 100);
+Console.WriteLine(numberC);
+if (numberA > numberB && numberA > numberC){
+	Console.WriteLine($"Это число самое большое из всех чисел: {numberA}");
+}
+else if (numberB > numberA && numberB > numberC){
+	Console.WriteLine($"Это число самое большое из всех чисел: {numberB}");
+}
+else if (numberC > numberA && numberC > numberB){
+	Console.WriteLine($"Это число самое большое из всех чисел: {numberC}");
+}
