eff5560b06663dc2812df6db91a9a6cb2f74f4ab
anton <antonsuslov.90@mail.ru>
2022-12-19 21:48:57 +0300

    done task1

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите любое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе любое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB) {
+	Console.WriteLine($"Результат: {numberA} больше, чем число {numberB}");
+}
+else {
+	Console.WriteLine($"Результат: {numberB} больше, чем число {numberA}");
+}
