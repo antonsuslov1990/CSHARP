bea4869b8ce8a5ce7e607fb9b374f1ddd5cba382
anton <antonsuslov.90@mail.ru>
2022-12-22 22:21:31 +0300

    task 1

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Console.Clear();
+int numberA = new Random().Next(100, 999);
+string n = numberA.ToString();
+Console.WriteLine(numberA);
+Console.WriteLine(n[1]); */	
+
+
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+int numberA = new Random().Next(100, 999);
+Console.WriteLine(numberA);
+
+numberA = numberA / 10;
+numberA = numberA % 10;
+
+Console.WriteLine(numberA);
