844c3d1fcfce2499e2a28c3eaa662d3277449571
anton <antonsuslov.90@mail.ru>
2022-12-23 18:18:49 +0300

    done task3

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("Введите день недели, что бы определить Рабочий это день или Выхондой");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+if (dayWeek > 0 && dayWeek < 8)
+    {
+        if (dayWeek == 7 || dayWeek == 6)
+        {
+            Console.Write($"Под цифрой {dayWeek} - Выходной");
+        }
+        else
+        {
+            Console.Write($"Под цифрой {dayWeek} - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    }	
