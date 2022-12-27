557bbf65bf5c5b678da04d82226573e6b5d2221d
anton <antonsuslov.90@mail.ru>
2022-12-27 19:33:41 +0300

    done task2

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+double sum = 0;
+for ( i = 1; i <= n; i++)
+	Console.WriteLine(sum = Math.Pow(i, 3));
