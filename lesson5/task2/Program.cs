08e90c1ef9b58324be3921e6087a8ece9e273838
anton <antonsuslov.90@mail.ru>
2023-01-03 18:31:47 +0300

    done task 2

---
+++ lesson5/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+int amountNumber = new Random().Next(5, 10);
+int[] array = new int[amountNumber];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 10);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int sumNumberUneven = 0;
+for (int i = 1; i < array.Length; i += 2)
+{
+	sumNumberUneven =  sumNumberUneven + (array[i]);
+}
+Console.WriteLine(sumNumberUneven);
