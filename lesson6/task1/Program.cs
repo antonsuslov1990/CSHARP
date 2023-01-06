822ee02bef993c3fa119c23c21f204a61bb2b6f3
anton <antonsuslov.90@mail.ru>
2023-01-06 15:09:19 +0300

    done task

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.WriteLine("Введите массив чисел: ");
+	for (int i = 0; i < n; i++)
+	{
+	array[i] = Convert.ToInt32(Console.ReadLine());
+	}
+
+int sumPositiveNumber = 0;
+
+for (int i = 0; i < n; i++)
+{
+	if (array[i] > 0)
+		sumPositiveNumber++;
+
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Положительных числе в данном массиве: {sumPositiveNumber}");
