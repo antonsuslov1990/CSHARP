66a40c100b27ac97a451e9c27347b0a650b03f64
anton <antonsuslov.90@mail.ru>
2023-01-12 19:09:15 +0300

    done task4

---
+++ lesson8/task4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// Функция вывода индекса элементов 3D массива
+void PrintIndexArray(int[,,] matrix3D)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+// Функция заполнения 3D массива
+void FillArray(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+Console.Write("Введите 3 координаты 3D матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,,] matrix3D = new int[size[0], size[1] , size[2]];
+Console.WriteLine();
+FillArray(matrix3D);
+PrintIndexArray(matrix3D);
